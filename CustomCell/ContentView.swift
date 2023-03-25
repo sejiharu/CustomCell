3a1de216917c8bc43a2b287a7257def3a88e221a
瀬島大生 <sejisoccer@keio.jp>
2023-03-26 04:16:04 +0900

    first commit

---
+++ CustomCell/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomCell
+//
+//  Created by 瀬島大生 on 2023/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomCell/CustomCellApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCellApp.swift
+//  CustomCell
+//
+//  Created by 瀬島大生 on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CustomCellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
