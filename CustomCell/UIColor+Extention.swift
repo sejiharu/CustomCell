7efb9338a0282f370a9e7ec73c01a6d870df48b2
瀬島大生 <sejisoccer@keio.jp>
2023-03-26 23:10:16 +0900

    :

---
+++ CustomCell/UIColor+Extention.swift
@@ -0,0 +1,32 @@
+//
+//  UIColor+Extention.swift
+//  CustomCell
+//
+//  Created by 瀬島大生 on 2023/03/26.
+//
+import UIKit
+import SwiftUI
+
+extension UIColor {
+    
+    static let flatDarkBackground = UIColor(red: 36, green: 36, blue: 36)
+    static let flatDarkCardBackground = UIColor(red: 46, green: 46, blue: 46)
+    
+    convenience init(red: Int, green: Int, blue: Int, a: CGFloat = 1.0) {
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: a)
+    }
+}
+
+extension Color {
+    public init(decimalRed red: Double, green: Double, blue: Double) {
+        self.init(red: red / 255, green: green / 255, blue: blue / 255)
+    }
+    
+    public static var flatDarkBackground: Color {
+        return Color(decimalRed: 36, green: 36, blue: 36)
+    }
+    
+    public static var flatDarkCardBackground: Color {
+        return Color(decimalRed: 46, green: 46, blue: 46)
+    }
+}
